325c3b332d8458f355146d58a8015abda3a3a9b7
Harishkumar N M <harishkumarnm0311@gmail.com>
2025-05-16 12:37:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int kthFactor(int n, int k) {
+       int[]ans=new int[4];
+       int index=0;
+       for(int i=1;i<n+1;i++){
+        if(n%i==0){
+            ans[index]=i;
+            index++;
+        }
+        if(index>=k){
+            break;
+        }
+       
+    }
+      if(ans[k-1]==0){
+        return -1;
+      }else{
+        return ans[k-1];
+      }
+    }
+}
