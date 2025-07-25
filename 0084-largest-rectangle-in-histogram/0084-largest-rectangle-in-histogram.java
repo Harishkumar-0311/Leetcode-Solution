f20461460b8067cbfac909a738ff2a981af359b4
Harishkumar N M <harishkumarnm0311@gmail.com>
2025-07-25 15:32:13 +0530

    Time: 6 ms (99.82%), Space: 59.6 MB (10.84%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,43 +1,21 @@
-public class Solution {
+class Solution {
     public int largestRectangleArea(int[] heights) {
-        int n = heights.length;
-        int[] leftSmall = new int[n];
-        int[] rightSmall = new int[n];
-        Stack<Integer> st = new Stack<>();
+    int n = heights.length;
+    int[] stack = new int[n + 1];
+    int top = -1;
+    int maxArea = 0;
 
-        for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
-                st.pop();
-            }
-            if (st.isEmpty()) {
-                leftSmall[i] = 0;
-            } else {
-                leftSmall[i] = st.peek() + 1;
-            }
-            st.push(i);
+    for (int i = 0; i <= n; i++) {
+        int h = (i == n) ? 0 : heights[i];
+        while (top != -1 && h < heights[stack[top]]) {
+            int height = heights[stack[top--]];
+            int width = (top == -1) ? i : i - stack[top] - 1;
+            maxArea = Math.max(maxArea, height * width);
         }
-        st.clear();
-
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
-                st.pop();
-            }
-            if (st.isEmpty()) {
-                rightSmall[i] = n - 1;
-            } else {
-                rightSmall[i] = st.peek() - 1;
-            }
-            st.push(i);
-        }
-
-        int maxArea = 0;
-        for (int i = 0; i < n; i++) {
-            int width = rightSmall[i] - leftSmall[i] + 1;
-            int area = heights[i] * width;
-            maxArea = Math.max(maxArea, area);
-        }
-
-        return maxArea;
+        stack[++top] = i;
     }
+
+    return maxArea;
+}
 
 }
