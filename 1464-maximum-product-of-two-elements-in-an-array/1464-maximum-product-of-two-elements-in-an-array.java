f89529ae6063657c618d89fcea21ad494078b21a
Harishkumar N M <harishkumarnm0311@gmail.com>
2025-07-31 07:36:15 +0530

    Time: 0 ms (100%), Space: 42.9 MB (39.19%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
@@ -1,34 +1,31 @@
 class Solution {
     public int maxProduct(int[] nums) {
-    //    int max=0;
-    //    int val=0;
-    //    for(int i=0;i<nums.length;i++){
-    //     for(int j=i;j<nums.length;j++){
-    //        if(i!=j){
-    //      val=(nums[i]-1)*(nums[j]-1);
-    //        }
-    //         if(val>max){
-    //             max=val;
-    //         }
-    //     }
-    //    } 
-    //    return max;
-    int val=0;
-       int max=0;
-       for (int i=0; i<nums.length;i++)
-       {
-        if(max<=nums[i])
-        {
-            val=max;
-            max=nums[i];
+        //    int max=0;
+        //    int val=0;
+        //    for(int i=0;i<nums.length;i++){
+        //     for(int j=i;j<nums.length;j++){
+        //        if(i!=j){
+        //      val=(nums[i]-1)*(nums[j]-1);
+        //        }
+        //         if(val>max){
+        //             max=val;
+        //         }
+        //     }
+        //    } 
+        //    return max;
+        int smax = 0;
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (max <= nums[i]) {
+                smax = max;
+                max = nums[i];
 
-        }else if (val<nums[i])
-        {
-            val=nums[i];
+            } else if (smax < nums[i]) {
+                smax = nums[i];
 
+            }
         }
-       }
-        int ans=(max-1)*(val-1);
-        return ans;
+        int val = (max - 1) * (smax - 1);
+        return val;
     }
 }
